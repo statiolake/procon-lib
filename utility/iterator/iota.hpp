196e143f49754bb3931caa42ce5de6f60e15ab85
statiolake <statiolake@gmail.com>
2018-05-26 23:05:41 +0900

    format using clang-format

--- utility/iterator/iota.hpp
+++ utility/iterator/iota.hpp
@@ -7,47 +7,54 @@ namespace pcl { namespace utility { namespace iterator {
 
     template <typename T>
     class iota {
-        private:
-            T m_beg, m_end, m_step;
-        public:
-            class iota_iterator {
-                private:
-                    T m_value, m_step;
-                public:
-                    iota_iterator(T value, T step) : m_value(value), m_step(step) {}
-                    bool operator!=(iota_iterator const &other) {
-                        return m_value != other.m_value;
-                    }
-                    bool operator==(iota_iterator const &other) {
-                        return m_value == other.m_value;
-                    }
-                    operator T() { return m_value; }
-                    T operator*() { return m_value; }
-                    iota_iterator &operator++() {
-                        m_value += m_step;
-                        return *this;
-                    }
-                    iota_iterator operator++(int) {
-                        iota_iterator orig = *this;
-                        m_value += m_step;
-                        return orig;
-                    }
-                    iota_iterator &operator--() {
-                        m_value -= m_step;
-                        return *this;
-                    }
-                    iota_iterator operator--(int) {
-                        iota_iterator orig = *this;
-                        m_value -= m_step;
-                        return orig;
-                    }
-            };
+      private:
+        T m_beg, m_end, m_step;
 
-            iota(T beg, T end, T step) : m_beg(beg), m_end(end), m_step(step) {
-                assert(m_step == 1 || m_step == -1);
+      public:
+        class iota_iterator {
+          private:
+            T m_value, m_step;
+
+          public:
+            iota_iterator(T value, T step)
+                : m_value(value)
+                , m_step(step) {}
+            bool operator!=(iota_iterator const &other) {
+                return m_value != other.m_value;
             }
-            iota_iterator begin() { return iota_iterator(m_beg, m_step); }
-            iota_iterator end() { return iota_iterator(m_end, 0); }
+            bool operator==(iota_iterator const &other) {
+                return m_value == other.m_value;
+            }
+            operator T() { return m_value; }
+            T operator*() { return m_value; }
+            iota_iterator &operator++() {
+                m_value += m_step;
+                return *this;
+            }
+            iota_iterator operator++(int) {
+                iota_iterator orig = *this;
+                m_value += m_step;
+                return orig;
+            }
+            iota_iterator &operator--() {
+                m_value -= m_step;
+                return *this;
+            }
+            iota_iterator operator--(int) {
+                iota_iterator orig = *this;
+                m_value -= m_step;
+                return orig;
+            }
+        };
+
+        iota(T beg, T end, T step)
+            : m_beg(beg)
+            , m_end(end)
+            , m_step(step) {
+            assert(m_step == 1 || m_step == -1);
+        }
+        iota_iterator begin() { return iota_iterator(m_beg, m_step); }
+        iota_iterator end() { return iota_iterator(m_end, 0); }
     };
 
     template <typename T>
@@ -60,6 +67,6 @@ namespace pcl { namespace utility { namespace iterator {
         return iota<T>(beg, end, -1);
     }
 
-}}}
+}}} // namespace pcl::utility::iterator
 
 #endif /* end of include guard: IOTA_HPP_GQIXM5SA */
