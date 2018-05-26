12873c09cea7d164042317668a2044cf492cf55d
statiolake <statiolake@gmail.com>
2018-05-27 00:49:11 +0900

    add utility/debug

--- prelude.hpp
+++ prelude.hpp
@@ -1,4 +1,6 @@
 #include <limits>
-using ll = long long;
+using ll          = long long;
 constexpr int INF = std::numeric_limits<int>::max() / 2;
-constexpr ll LNF = std::numeric_limits<ll>::max() / 2;
+constexpr ll LNF  = std::numeric_limits<ll>::max() / 2;
+
+#include "utility/debug.hpp"
---
+++ utility/debug.hpp
@@ -0,0 +1,10 @@
+#ifndef DEBUG_HPP_OB5Y82L9
+#define DEBUG_HPP_OB5Y82L9
+
+#ifdef PA_DEBUG
+#define PD(stmt) stmt
+#else
+#define PD(stmt)
+#endif
+
+#endif /* end of include guard: DEBUG_HPP_OB5Y82L9 */
