621ca57d79253d7e10890af8347ba503a65465db
statiolake <statiolake@gmail.com>
2018-06-09 10:57:11 +0900

    add utility/stream_helper.hpp to prelude.hpp

--- prelude.hpp
+++ prelude.hpp
@@ -4,3 +4,4 @@ constexpr int INF = std::numeric_limits<int>::max() / 2;
 constexpr ll LNF  = std::numeric_limits<ll>::max() / 2;
 
 #include "utility/debug.hpp"
+#include "utility/stream_helper.hpp"
