b82062e1aae3c7daec6570701f3ca87303411716
statiolake <statiolake@gmail.com>
2018-06-09 19:52:20 +0900

    prelude: add DX / DY

--- prelude.hpp
+++ prelude.hpp
@@ -1,7 +1,9 @@
 #include <limits>
-using ll          = long long;
-constexpr int INF = std::numeric_limits<int>::max() / 2;
-constexpr ll LNF  = std::numeric_limits<ll>::max() / 2;
+using ll           = long long;
+constexpr int INF  = std::numeric_limits<int>::max() / 2;
+constexpr ll LNF   = std::numeric_limits<ll>::max() / 2;
+constexpr int DX[] = {1, 0, -1, 0, 1, 1, -1, -1};
+constexpr int DY[] = {0, 1, 0, -1, -1, 1, 1, -1};
 
 #include "utility/debug.hpp"
 #include "utility/stream_helper.hpp"
