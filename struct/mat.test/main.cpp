8c3ae6fe4ac985d1b820ee834dc9869f5873afeb
statiolake <statiolake@gmail.com>
2018-12-12 03:14:21 +0900

    struct/mat: add test

---
+++ struct/mat.test/main.cpp
@@ -0,0 +1,27 @@
+/// basically verified with: aoj:ITP1_7_D
+
+#include "prelude.hpp"
+
+#include "struct/mat.hpp"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace pcl;
+
+int main() {
+    int n, m, l;
+    cin >> n >> m >> l;
+
+    mat<ll> A(n, m), B(m, l);
+    cin >> A >> B;
+
+    auto C = A * B;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < l; j++) {
+            if (j != 0) cout << ' ';
+            cout << C.at(i, j);
+        }
+        cout << endl;
+    }
+}
