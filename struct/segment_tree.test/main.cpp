b7cd11a51fadfc789e1711e167916464a5cc2015
statiolake <statiolake@gmail.com>
2018-12-11 23:54:10 +0900

    struct/segment_tree: add test

---
+++ struct/segment_tree.test/main.cpp
@@ -0,0 +1,24 @@
+#include "prelude.hpp"
+
+#include "struct/segment_tree.hpp"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace pcl;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    segment_tree<range_minimum_query<ll>> st(n, (1ll << 31) - 1);
+    for (int i = 0; i < q; i++) {
+        int com, x, y;
+        cin >> com >> x >> y;
+        if (com == 0) {
+            st.update(x, y);
+        } else {
+            cout << st.find(x, y + 1) << endl;
+        }
+    }
+}
