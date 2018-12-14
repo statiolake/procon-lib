ea36b1eb9dc50927e7ba037fee04d0ac87581121
statiolake <statiolake@gmail.com>
2018-12-15 02:48:34 +0900

    util/alias: add strict_in_range

--- util/alias.hpp
+++ util/alias.hpp
@@ -20,9 +20,17 @@ template <typename T, typename U, typename V>
 bool in_range(T const &begin, U const &mid, V const &end) {
     return begin <= mid && mid < end;
 }
+
 template <typename T, typename U, typename V>
 bool in_range(T const &begin, std::initializer_list<U> list, V const &end) {
     auto p = std::minmax_element(list.begin(), list.end());
     return begin <= *p.first && *p.second < end;
+}
+
+template <typename T, typename U, typename V>
+bool strict_in_range(T const &begin, std::initializer_list<U> list,
+                     V const &end) {
+    if (!std::is_sorted(list.begin(), list.end())) return false;
+    return in_range(begin, list, end);
 }
 } // namespace pcl
