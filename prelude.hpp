7b59032441d4a8df9ec77683fd82eabb7ce35894
statiolake <statiolake@gmail.com>
2018-12-20 18:09:11 +0900

    prelude: add doc

--- prelude.hpp
+++ prelude.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file prelude.hpp
+ * @brief どのコードでも必ず使うだろうものを宣言またはインクルードする。
+ * `procon-assistant init` で生成されるファイルのテンプレートにはデフォルトで
+ * `#include "prelude.hpp"`が含まれている。また、全ての他のヘッダはこのファイ
+ * ルはインクルードされているものとしてよい。(もちろん各ヘッダがそれぞれ自分で
+ * インク ルードすることを推奨する。)
+ */
 #pragma once
 
 using ll  = long long;
