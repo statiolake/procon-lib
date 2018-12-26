64301a579f2d46f9f65e8b3ff50939c2a1795a9c
statiolake <statiolake@gmail.com>
2018-12-26 17:52:32 +0900

    util/stream/overloads: add doc

--- util/stream/overloads.hpp
+++ util/stream/overloads.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file util/stream/overloads.hpp
+ * @brief
+ * いくつかの型について、ストリーム入出力演算子をオーバーロードしている。
+ */
 #pragma once
 
 #include "../../prelude.hpp"
@@ -5,6 +10,12 @@
 #include <iostream>
 #include <vector>
 namespace pcl {
+
+/**
+ * @brief vector<T> の出力演算子
+ * @param[in] v 出力したい vector
+ * @return std::ostream & 出力ストリーム
+ */
 template <typename T>
 std::ostream &operator<<(std::ostream &os, std::vector<T> const &v) {
     os << '[';
@@ -16,9 +27,18 @@ std::ostream &operator<<(std::ostream &os, std::vector<T> const &v) {
     return os;
 }
 
+/**
+ * @brief vector<T> の入力演算子
+ * @param[in] v 入力したい対象の vector
+ * @return std::istream & 入力ストリーム
+ * @details 空白区切りの入力を、 vector
+ * の長さの回数だけ受け取って、前から順につめていく関数。 push_back()
+ * ではないので、既存の要素を上書きする。
+ */
 template <typename T>
 std::istream &operator>>(std::istream &is, std::vector<T> &v) {
     for (size_t i = 0; i < v.size(); i++) is >> v[i];
     return is;
 }
+
 } // namespace pcl
