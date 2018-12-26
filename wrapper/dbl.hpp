decf85a7dbbc6f66123971251d27d21b233dc8ab
statiolake <statiolake@gmail.com>
2018-12-27 00:14:39 +0900

    wrapper/dbl: add doc

--- wrapper/dbl.hpp
+++ wrapper/dbl.hpp
@@ -1,3 +1,7 @@
+/**
+ * @file wrapper/dbl.hpp
+ * @brief 誤差を許容する比較を定義した浮動小数点数。
+ */
 #pragma once
 
 #include "../prelude.hpp"
@@ -7,8 +11,16 @@
 #include <type_traits>
 
 namespace pcl {
+
+/**
+ * @brief 誤差を許容する比較を定義した浮動小数点数。
+ * @details プリミティブの double とできるだけ同じ操作感を得ることを目指してい
+ * る。オーバーロードを用意したため dbl と double の比較も可能。このとき
+ * double は dbl 型に変換される。
+ */
 class dbl {
   private:
+    /// 値
     double v;
 
     friend bool operator==(dbl const &a, dbl const &b);
@@ -19,15 +31,20 @@ class dbl {
     friend bool operator>=(dbl const &a, dbl const &b);
 
   public:
+    /// デフォルトコンストラクタ
     dbl()
         : v() {
     }
+
+    /// 指定された値で初期化するコンストラクタ
     dbl(double init)
         : v(init) {
     }
-    dbl(dbl const &o)
-        : v(o.v) {
-    }
+
+    /// コピーコンストラクタ
+    dbl(dbl const &o) = default;
+
+    /// double へのキャスト演算子
     explicit operator double() const {
         return v;
     }
@@ -61,31 +78,54 @@ class dbl {
 inline dbl operator+(dbl a, dbl const &b) {
     return (a += b);
 }
+
 inline dbl operator-(dbl a, dbl const &b) {
     return (a -= b);
 }
+
 inline dbl operator*(dbl a, dbl const &b) {
     return (a *= b);
 }
+
 inline dbl operator/(dbl a, dbl const &b) {
     return (a /= b);
 }
 
+/**
+ * @brief 二者が誤差を許容して等しければ真となる。
+ * @details 二者の絶対値の差が `EPS` より小さければ真となる。
+ */
 inline bool operator==(dbl const &a, dbl const &b) {
     return std::abs(a.v - b.v) < EPS;
 }
+
+/// 単に `==` を反転したもの。
 inline bool operator!=(dbl const &a, dbl const &b) {
     return !(a == b);
 }
+
+/// 真に小さいか、二者が誤差を許容して等しければ真となる。
 inline bool operator<=(dbl const &a, dbl const &b) {
     return a.v < b.v || a == b;
 }
+
+/// 真に大きいか、二者が誤差を許容して等しければ真となる。
 inline bool operator>=(dbl const &a, dbl const &b) {
     return a.v > b.v || a == b;
 }
+
+/**
+ * @brief 単に `>=` を反転したもの。
+ * @details 真に小さく、かつ、二者が誤差を許容しても等しくなければ真となる。
+ */
 inline bool operator<(dbl const &a, dbl const &b) {
     return !(a >= b);
 }
+
+/**
+ * @brief 単に `<=` を反転したもの。
+ * @details 真に大きく、かつ、二者が誤差を許容しても等しくなければ真となる。
+ */
 inline bool operator>(dbl const &a, dbl const &b) {
     return !(a <= b);
 }
@@ -93,12 +133,15 @@ inline bool operator>(dbl const &a, dbl const &b) {
 inline dbl operator+(dbl const &a, double b) {
     return a + dbl(b);
 }
+
 inline dbl operator-(dbl const &a, double b) {
     return a - dbl(b);
 }
+
 inline dbl operator*(dbl const &a, double b) {
     return a * dbl(b);
 }
+
 inline dbl operator/(dbl const &a, double b) {
     return a / dbl(b);
 }
@@ -106,18 +149,23 @@ inline dbl operator/(dbl const &a, double b) {
 inline bool operator==(dbl const &a, double b) {
     return a == dbl(b);
 }
+
 inline bool operator!=(dbl const &a, double b) {
     return a != dbl(b);
 }
+
 inline bool operator<(dbl const &a, double b) {
     return a < dbl(b);
 }
+
 inline bool operator>(dbl const &a, double b) {
     return a > dbl(b);
 }
+
 inline bool operator<=(dbl const &a, double b) {
     return a <= dbl(b);
 }
+
 inline bool operator>=(dbl const &a, double b) {
     return a >= dbl(b);
 }
@@ -125,12 +173,15 @@ inline bool operator>=(dbl const &a, double b) {
 inline dbl operator+(double a, dbl const &b) {
     return dbl(a) + b;
 }
+
 inline dbl operator-(double a, dbl const &b) {
     return dbl(a) - b;
 }
+
 inline dbl operator*(double a, dbl const &b) {
     return dbl(a) * b;
 }
+
 inline dbl operator/(double a, dbl const &b) {
     return dbl(a) / b;
 }
@@ -138,22 +189,28 @@ inline dbl operator/(double a, dbl const &b) {
 inline bool operator==(double a, dbl const &b) {
     return dbl(a) == b;
 }
+
 inline bool operator!=(double a, dbl const &b) {
     return dbl(a) != b;
 }
+
 inline bool operator<(double a, dbl const &b) {
     return dbl(a) < b;
 }
+
 inline bool operator>(double a, dbl const &b) {
     return dbl(a) > b;
 }
+
 inline bool operator<=(double a, dbl const &b) {
     return dbl(a) <= b;
 }
+
 inline bool operator>=(double a, dbl const &b) {
     return dbl(a) >= b;
 }
 
+/// ストリーム入力イテレータ
 std::istream &operator>>(std::istream &is, dbl &d) {
     double v;
     is >> v;
@@ -161,20 +218,29 @@ std::istream &operator>>(std::istream &is, dbl &d) {
     return is;
 }
 
+/// ストリーム出力イテレータ
 std::ostream &operator<<(std::ostream &os, dbl const &d) {
     os << (double)d;
     return os;
 }
+
 } // namespace pcl
 
 namespace std {
+
+/// 平方根を求める。
 pcl::dbl sqrt(pcl::dbl const &a) {
     return pcl::dbl(sqrt(static_cast<double>(a)));
 }
+
+/// 最小値を求める。
 pcl::dbl min(pcl::dbl const &a, pcl::dbl const &b) {
     return a < b ? a : b;
 }
+
+/// 最大値を求める。
 pcl::dbl max(pcl::dbl const &a, pcl::dbl const &b) {
     return a < b ? b : a;
 }
+
 } // namespace std
